20688d3e703915de50fc16073e36c5e80a3f4bc7
AJ Taylor <anthonjtaylor@gmail.com>
2018-09-05 22:58:20 -0700

    add basic lexer analyzer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -4,6 +4,10 @@ import (
 	"monkey/token"
 )
 
+//a lexer has input
+//has a position
+//has a readPosition
+//has the actual charachter which is a byte
 type Lexer struct {
 	input        string
 	position     int
@@ -18,18 +22,25 @@ func New(input string) *Lexer {
 }
 
 func (l *Lexer) readChar() {
+	//check length of input and return 0 if it's at the end
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
+		//else the ch that it's at is the readposition of the input
 		l.ch = l.input[l.readPosition]
 	}
+	//position is now readposition
 	l.position = l.readPosition
+	//increment readposition while you're at it
 	l.readPosition += 1
 }
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhiteSpace()
+	//find out what the charachter is
+	//chreate a new token with the token type and literal
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -50,11 +61,64 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+
+			//get literal for ident
+			tok.Literal = l.readIdentifier()
+
+			//check if the ident literal is a reserved word
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			//token is illegal and not a letter or anything we've defined
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
 }
 
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+
+	//loop until no longer a letter
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	//return the identifer form starting position to end positon
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- token/token.go
+++ token/token.go
@@ -7,6 +7,11 @@ type Token struct {
 	Literal string
 }
 
+var keywords = map[string]TokenType{
+	"let": LET,
+	"fn":  FUNCTION,
+}
+
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -31,3 +36,12 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+func LookupIdent(ident string) TokenType {
+	//if the ident a reserved/defined type in the map, return that type
+	//if it is not in there, return that it's an ident and not some reserved type
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
